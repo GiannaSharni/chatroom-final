b7c4627b207147182f8dd7747a8bcd777836a4cf
Gianna Sharni <GiannaSharni@gmail.com>
2024-05-05 19:52:16 +0100

    completed

--- app/javascript/controllers/chatroom_subscription_controller.js
+++ app/javascript/controllers/chatroom_subscription_controller.js
@@ -9,8 +9,13 @@ export default class extends Controller {
   connect() {
     this.subscription = createConsumer().subscriptions.create(
       { channel: "ChatroomChannel", id: this.chatroomIdValue },
-      { received: data => console.log(data) }
+      { received: (data) => this.#insertMessage(data)}
     )
-    console.log(`Subscribed to the chatroom with the id ${this.chatroomIdValue}.`)
+    // console.log("this.messageTarget:", this.messageTarget);
+  }
+
+  #insertMessage(data) {
+    this.messagesTarget.insertAdjacentHTML("beforeend", data)
+    this.messagesTarget.scrollTo(0, this.messagesTarget.scrollHeight)
   }
 }
