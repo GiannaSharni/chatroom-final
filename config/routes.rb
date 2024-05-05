974db0ae9c023a2a555ab245b7a788d81a68bd7b
Gianna Sharni <GiannaSharni@gmail.com>
2024-05-05 19:02:31 +0100

    rails new

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "pages#home"
+  root to: "chatrooms#index"
+  resources :chatrooms, only: [:index, :show] do
+    resources :messages, only: :create
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -9,4 +12,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+
 end
